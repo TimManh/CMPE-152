c7f931e27b2b9bdf9fb58c1d404d98e15e29db3c
bob <timmanh93@gmail.com>
2020-02-04 11:21:14 -0800

    Output files and input files

--- First_assignment/assignment1.pas
+++ First_assignment/assignment1.pas
@@ -1,9 +1,12 @@
 Program HelloWorld(output);
+{Declare variable i to start the loop}
 var
     i: integer;
 begin
+	{Start the loop with starting point from 1 to 10}
     for i:=1 to 10 do
     begin
-        writeln('Hello, world!');
+		{Print out 10 times HelloWorld}
+        writeln('Hello world!');
     end;
 end.
